22dd1db5ed85c80c1098e7465d69ff925e6948bc
Nikita Ivanov <nikita.ivanov@truesalessoft.com>
2022-11-13 20:12:55 +0300

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,10 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
@@ -11,13 +13,16 @@ import { Router } from 'react-router-dom';
 
 export const history = createBrowserHistory();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <StoreContext.Provider value={store}>
     <Router history={history}>
       <App />
     </Router>
-  </StoreContext.Provider>,
-  document.getElementById('root')
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
