5226ed095e4c759e12ae5d5f7b7d8e0c8beeef3c
Nikita Ivanov <nikita.ivanov@truesalessoft.com>
2023-01-04 22:13:28 +0300

    #changed
    Comment: delete loading spinner on float action button

--- client-app/src/app/layout/FloatingAction.tsx
+++ client-app/src/app/layout/FloatingAction.tsx
@@ -8,6 +8,10 @@ import React from 'react';
 const actions = [{ icon:  <SportsBarIcon/> , name: 'Create Activity', path: '/createActivity'}];
 
 export default observer(function FloatingAction() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const history = useHistory();
     const{activityStore, commonStore, profileStore} = useStore();
     const{loadingInitial} = activityStore;
@@ -15,26 +19,32 @@ export default observer(function FloatingAction() {
     const{loadingProfile} = profileStore;
 
     function handleRederect(path: string) {
+        handleClose()
         history.push(path);
+        handleClose()
     }
 
     return (<>
     {!loadingInitial && 
     !loadingProfile &&
-    appLoaded &&
-        <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-        icon={<SpeedDialIcon/>}
-        >
-        {actions.map((action) => (
-            <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            onClick={() => handleRederect(action.path)}
-            />
-        ))}
-        </SpeedDial>}
-        </>)
+    appLoaded && 
+            <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            sx={{ position: 'fixed', bottom: 16, right: 16 }}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            open={open}
+            icon={<SpeedDialIcon/>}
+            >
+            {actions.map((action) => (
+                <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={() => handleRederect(action.path)}
+                />
+            ))}
+            </SpeedDial>
+    }
+    </>)
 })
